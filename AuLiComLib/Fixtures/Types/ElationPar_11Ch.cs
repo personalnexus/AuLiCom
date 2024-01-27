39c05c01e1dfa02526c7c25f6f8eda4cfdf5e7d0
StefanH <personalnexus@users.noreply.github.com>
2024-01-27 09:10:14 +0100

    Added two new fixtures

---
+++ AuLiComLib/Fixtures/Types/ElationPar_11Ch.cs
@@ -0,0 +1,25 @@
+﻿using AuLiComLib.Colors.Channels;
+using AuLiComLib.Protocols;
+
+namespace AuLiComLib.Fixtures.Types
+{
+    public class ElationPar_11Ch : FixtureBase, IFixture
+    {
+        public ElationPar_11Ch(IConnection connection) : base(connection)
+        {
+        }
+
+        public ChannelValueProperty Intensity { get; set; }
+        public ChannelValueProperty DimmerFine { get; set; }
+        public ChannelValueProperty Strobe { get; set; }
+
+        public RedChannelValueProperty Red { get; set; }
+        public GreenChannelValueProperty Green { get; set; }
+        public BlueChannelValueProperty Blue { get; set; }
+
+        public ChannelValueProperty Ignore1 { get; set; }
+        public ChannelValueProperty Ignore2 { get; set; }
+        public ChannelValueProperty Cto { get; set; }
+        public ChannelValueProperty Ignore3 { get; set; }
+    }
+}
